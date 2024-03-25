a7b183192257b5c1f232fe78b3654fe9c6f09222
lasha-codes <lashagiorgi420@gmail.com>
2024-03-25 10:19:11 -0700

    learned how to connect postgres to node server very cool

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const db = require('./db')
 
 const app = express()
 
 app.use(express.json())
 
 // get all restaurants
-app.get('/api/v1/restaurants', (req, res) => {
+app.get('/api/v1/restaurants', async (req, res) => {
+  const results = await db.query('SELECT * FROM restaurants')
+  console.log(results)
   res.status(200).json({
     status: 'success',
     data: { restaurants: ['restaurant', 'man', 'mcdonald', 'wendy'] },
