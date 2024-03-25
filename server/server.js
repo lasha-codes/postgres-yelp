157305499df39909e7810a013889c608bb1cf3dc
lasha-codes <lashagiorgi420@gmail.com>
2024-03-25 09:45:25 -0700

    just configuring routes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 // get all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
-  res.status(404).json({
+  res.status(200).json({
     status: 'success',
     data: { restaurants: ['restaurant', 'man', 'mcdonald', 'wendy'] },
   })
@@ -14,6 +16,43 @@ app.get('/api/v1/restaurants', (req, res) => {
 // get a restaurant
 app.get('/api/v1/restaurants/:id', (req, res) => {
   console.log(req.params)
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurant: 'mcdonald',
+    },
+  })
+})
+
+// create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+  res.json({
+    status: 'success',
+    data: {
+      restaurant: 'mcdonald',
+    },
+  })
+})
+
+// update a restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id)
+  console.log(req.body)
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurant: 'macdonald',
+    },
+  })
+})
+
+// delete restaurant
+
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+  res.status(204).json({
+    status: 'success',
+  })
 })
 
 const port = process.env.PORT || 3001
