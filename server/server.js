a4761369a7eac7d85ee6bb0daca59a825d292343
lasha-codes <lashagiorgi420@gmail.com>
2024-03-26 05:05:00 -0700

    update a restaurant logic is done

--- server/server.js
+++ server/server.js
@@ -61,15 +61,24 @@ app.post('/api/v1/restaurants', async (req, res) => {
 })
 
 // update a restaurant
-app.put('/api/v1/restaurants/:id', (req, res) => {
-  console.log(req.params.id)
-  console.log(req.body)
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurant: 'macdonald',
-    },
-  })
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    )
+    console.log(results)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        restaurant: results.rows[0],
+      },
+    })
+    console.log(req.params.id)
+    console.log(req.body)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 // delete restaurant
