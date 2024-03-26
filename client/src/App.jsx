bd11e00edde373eefff4cb3af71c7ceaf0242104
lasha-codes <lashagiorgi420@gmail.com>
2024-03-26 06:42:57 -0700

    set up a global context so every component or page can have access to the same data.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home'
 import UpdatePage from './routes/UpdatePage'
 import RestaurantDetailPage from './routes/RestaurantDetailPage'
+import { RestaurantsContextProvider } from './context/RestaurantsContext'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/restaurants/:id/update' element={<UpdatePage />} />
-          <Route path='/restaurants/:id' element={<RestaurantDetailPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/restaurants/:id/update' element={<UpdatePage />} />
+            <Route path='/restaurants/:id' element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   )
 }
 
