d6f3f070b1b74307be3941972dd4530c7e646a7b
lasha-codes <lashagiorgi420@gmail.com>
2024-03-27 04:37:06 -0700

    make all of the input controlled

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,21 +1,45 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const UpdateRestaurant = () => {
   const { id } = useParams()
+
+  const [name, setName] = useState('')
+  const [location, setLocation] = useState('')
+  const [priceRange, setPriceRange] = useState('')
+
   return (
     <div className='px-5'>
       <form className='form-group row gap-3'>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input id='name' className='form-control' type='text' />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id='name'
+            className='form-control'
+            type='text'
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='location'>Location</label>
-          <input id='location' className='form-control' type='text' />
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            id='location'
+            className='form-control'
+            type='text'
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='price_range'>Price Range</label>
-          <input id='price_range' className='form-control' type='number' />
+          <input
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+            id='price_range'
+            className='form-control'
+            type='number'
+          />
         </div>
         <button className='btn btn-primary'>Submit</button>
       </form>
