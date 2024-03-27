f547a2eae709bf6a2fe77e1ae5d349ca6b8f0eef
lasha-codes <lashagiorgi420@gmail.com>
2024-03-27 04:33:44 -0700

    ui is done for update restaurant page not gotta work on the logic

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,9 +1,26 @@
 import { useParams } from 'react-router-dom'
 
 const UpdateRestaurant = () => {
-  const params = useParams()
-  console.log(params)
-  return <div></div>
+  const { id } = useParams()
+  return (
+    <div className='px-5'>
+      <form className='form-group row gap-3'>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input id='name' className='form-control' type='text' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='location'>Location</label>
+          <input id='location' className='form-control' type='text' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='price_range'>Price Range</label>
+          <input id='price_range' className='form-control' type='number' />
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default UpdateRestaurant
