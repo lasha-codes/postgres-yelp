06682664c07ad3ac50aeae761c654b21bdb80f8e
lasha-codes <lashagiorgi420@gmail.com>
2024-03-27 13:07:20 -0700

    learned how foreign keys work new type and added new reviews table

--- client/src/db/db.sql
+++ client/src/db/db.sql
@@ -1,8 +1,9 @@
 CREATE TABLE reviews (
    id BIGSERIAL NOT NULL PRIMARY KEY,
+   restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
    name VARCHAR(50) NOT NULL,
    review TEXT NOT NULL,
-   rating INT NOT NULL check(rating >= 1 and rating <= 5) 
+   rating INT NOT NULL check(rating >= 1 and rating <= 5)
 );
 
-INSERT INTO reviews (name, review, rating) values('carl', 'restaurant was awesome', 5) returning *;
+INSERT INTO reviews (restaurant_id, name, review, rating) values(11, 'carl', 'restaurant was awesome', 5) returning *;
