2a5b3c66b228196853b51a85497d5ae87acc5f61
lasha-codes <lashagiorgi420@gmail.com>
2024-03-29 05:44:24 -0700

    learning very new methods in sql

--- server/db/db.sql
+++ server/db/db.sql
@@ -7,3 +7,13 @@ CREATE TABLE reviews (
 );
 
 INSERT INTO reviews (restaurant_id, name, review, rating) values(11, 'carl', 'restaurant was awesome', 5) returning *;
+
+SELECT count(*) FROM reviews;
+SELECT count(rating) FROM reviews;
+SELECT MIN(rating) FROM reviews;
+SELECT MAX(rating) FROM reviews;
+SELECT trunc(AVG(rating), 2) FROM reviews;
+SELECT trunc(AVG(rating), 3) AS average_rating FROM reviews;
+SELECT trunc(AVG(rating), 2) AS average_rating FROM reviews WHERE restaurant_id = 14;
+SELECT location, count(location) FROM restaurants group by location;
+SELECT name AS username, rating AS restaurant_rating FROM reviews;
